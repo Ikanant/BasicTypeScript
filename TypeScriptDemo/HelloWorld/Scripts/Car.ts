173ab333c3382f8335be3c16458a3f384886aa9d
Ikanant <jonathan.hdez92@gmail.com>
2018-02-25 16:14:14 -0500

    first 2 basic examples

---
+++ TypeScriptDemo/HelloWorld/Scripts/Car.ts
@@ -0,0 +1,20 @@
+﻿class Car {
+    engine: string;
+
+    constructor(engine: string) {
+        this.engine = engine;
+    }
+
+    start() {
+        alert('Engine STARTED: ' + this.engine);
+    }
+    stop() {
+        alert('Engine STOPPED: ' + this.engine);
+    }
+}
+
+window.onload = function () {
+    var car = new Car('V8');
+    car.start();
+    car.stop();
+}
