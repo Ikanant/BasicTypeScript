533d84ad54ae3162c60cb1ee9f2f62f8cfba61ee
Ikanant <jonathan.hdez92@gmail.com>
2018-02-26 21:22:00 -0500

    Imported knockout.js and included its typings file in order to use IntelliSense

---
+++ TypeScriptTypes/TypeScriptTypes/Scripts/02-04-ambient-declaration.ts
@@ -0,0 +1,17 @@
+﻿/// <reference path="typings/knockout-2.2.d.ts" />
+module demo_02_04 {
+    // KO beloings to the imported Knockout nuget pacakge we installed
+    declare var ko: KnockoutStatic;
+    
+    var name = ko.observable('Jonathan Hdez');
+
+    var id = ko.observable(1);
+
+    var guy = {
+        id: id,
+        fullName: name
+    }
+
+    var value: string = guy.fullName();
+    console.log(value);
+}
